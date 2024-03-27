3ade74908bf6481bbf64dc741d261534ec5d52e2
Rodrigo França <rfrancacosta@gmail.com>
2024-03-26 23:34:20 -0300

    23. Ansible - Mão na massa - instalação do banco dados MySQL via Ansible

--- ansible-lab/control-node/provision.sh
+++ ansible-lab/control-node/provision.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 # instalando o repo do yum epel-release
+sudo yum -y update
 sudo yum -y install nano
 sudo yum -y install epel-release
 echo "Installing Ansible"
@@ -7,7 +8,7 @@ sudo yum -y install ansible
 # O comando cat permite a criação de novos arquivos de texto de forma rápida
 # O comando EOT permite colocar uma string dentro do /etc/hosts em um formato de quebra de linha
 cat <<EOT >> /etc/hosts
-192.168.1.2 control-node
-192.168.1.3 app-01
-192.168.1.4 db01
+192.168.33.2 control-node
+192.168.33.3 app-01
+192.168.33.4 db01
 EOT
